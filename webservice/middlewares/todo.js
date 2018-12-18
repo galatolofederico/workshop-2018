705d87900c8f1d76c606f302d576e9da75e3f6af
galatolofederico <galatolo.federico@gmail.com>
2018-12-18 17:22:07 +0100

    fine

--- webservice/middlewares/todo.js
+++ webservice/middlewares/todo.js
@@ -9,11 +9,13 @@ router.use(listloader)
 router.put("/:id", (req, res) => {
     req.list.list[req.params.id] = req.body.todo    
     req.list.saveList()
+    res.end()
 })
 
 router.delete("/:id", (req, res) => {
     req.list.list.splice(req.params.id, 1)
     req.list.saveList()
+    res.end()
 })
 
 
--- webservice/test/test.js
+++ webservice/test/test.js
@@ -31,7 +31,7 @@ describe("POST /list ", () => {
         .post("/list")
         .send({
             todo: {
-                value: "prova"
+                value: "prova 1"
             }
         })
         .end((err, res) => {
@@ -41,3 +41,65 @@ describe("POST /list ", () => {
         })
     })
 })
+
+describe("POST /list second time ", () => {
+    it("Should add another element to the list", done => {
+        chai.request(app)
+        .post("/list")
+        .send({
+            todo: {
+                value: "prova 2"
+            }
+        })
+        .end((err, res) => {
+            list.loadList()
+            list.list.length.should.be.eql(2)
+            done()
+        })
+    })
+})
+
+describe("seconda GET /list ", () => {
+    it("Should return an array with length 2", done => {
+        chai.request(app)
+        .get("/list")
+        .end((err, res) => {
+            res.body.list.should.be.a("array")
+            res.body.list.length.should.be.eql(2)
+            done()
+        })
+    })
+})
+
+describe("PUT /todo/1 ", () => {
+    it("Should modify the second element", done => {
+        chai.request(app)
+        .put("/todo/1")
+        .send({
+            todo: {
+                value: "NUOVO"
+            }
+        })
+        .end((err, res) => {
+            list.loadList()
+            list.list.length.should.be.eql(2)
+            list.list[1].value.should.be.eql("NUOVO")
+            done()
+        })
+    })
+})
+
+
+describe("DELETE /todo/1 ", () => {
+    it("Should delete the second element", done => {
+        chai.request(app)
+        .delete("/todo/1")
+        .end((err, res) => {
+            list.loadList()
+            list.list.length.should.be.eql(1)
+            list.list[0].value.should.be.eql("prova 1")
+            done()
+        })
+    })
+})
+
